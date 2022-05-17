0c7bf2defdc63b0bc452ec1391596a6a2fa6e051
Yaroslav Vlasenko <yaroslavvlasenko@gmail.com>
2022-05-17 19:59:54 +0200

    Let's Go book Chapter 6.1

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,10 +3,10 @@ package main
 import "net/http"
 
 // The routes() method returns a servemux containing our application routes.
-func (app *application) routes() *http.ServeMux {
-    mux := http.NewServeMux()
+func (app *application) routes() http.Handler {
+	mux := http.NewServeMux()
 
-    	// Create a file server which serves files out of the "./ui/static" directory.
+	// Create a file server which serves files out of the "./ui/static" directory.
 	// Note that the path given to the http.Dir function is relative to the project
 	// directory root.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -20,5 +20,8 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-    return mux
+	// Pass the servemux as the 'next' parameter to the secureHeaders middleware.
+	// Because secureHeaders is just a function, and the function returns a
+	// http.Handler we don't need to do anything else.
+	return secureHeaders(mux)
 }
