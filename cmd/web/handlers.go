1383ef4b5a57c9d7411245c88677dfe46d3ab1e2
Yaroslav Vlasenko <yaroslavvlasenko@gmail.com>
2022-05-17 11:15:24 +0200

    Let's Go book Chapter 4.7

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,79 +1,95 @@
 package main
 
 import (
-    "fmt"
-    "html/template"
-    "net/http"
-    "strconv"
+	"errors"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+
+	"github.com/YaroslavVlasenko/snippetbox/internal/models"
 )
 
 // Change the signature of the home handler so it is defined as a method against
 // *application.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        app.notFound(w) // Use the notFound() helper
-        return
-    }
+	if r.URL.Path != "/" {
+		app.notFound(w) // Use the notFound() helper
+		return
+	}
 
-    files := []string{
-        "./ui/html/base.tmpl",
-        "./ui/html/partials/nav.tmpl",
-        "./ui/html/pages/home.tmpl",
-    }
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
 
-    ts, err := template.ParseFiles(files...)
-    if err != nil {
-        // Because the home handler function is now a method against application
-        // it can access its fields, including the error logger. We'll write the log
-        // message to this instead of the standard logger.
-        app.errorLog.Println(err.Error())
-        app.serverError(w, err) // Use the serverError() helper.
-        return
-    }
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		// Because the home handler function is now a method against application
+		// it can access its fields, including the error logger. We'll write the log
+		// message to this instead of the standard logger.
+		app.errorLog.Println(err.Error())
+		app.serverError(w, err) // Use the serverError() helper.
+		return
+	}
 
-    err = ts.ExecuteTemplate(w, "base", nil)
-    if err != nil {
-        // Also update the code here to use the error logger from the application
-        // struct.
-        app.serverError(w, err) // Use the serverError() helper.
-    }
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		// Also update the code here to use the error logger from the application
+		// struct.
+		app.serverError(w, err) // Use the serverError() helper.
+	}
 }
 
 // Change the signature of the snippetView handler so it is defined as a method
 // against *application.
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.URL.Query().Get("id"))
-    if err != nil || id < 1 {
-        app.notFound(w) // Use the notFound() helper.
-        return
-    }
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w) // Use the notFound() helper.
+		return
+	}
 
-    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	// Use the SnippetModel object's Get method to retrieve the data for a
+	// specific record based on its ID. If no matching record is found,
+	// return a 404 Not Found response.
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 // Change the signature of the snippetCreate handler so it is defined as a method
 // against *application.
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPost {
-        w.Header().Set("Allow", http.MethodPost)
-        app.clientError(w, http.StatusMethodNotAllowed) // Use the clientError() helper.
-        return
-    }
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed) // Use the clientError() helper.
+		return
+	}
 
 	// Create some variables holding dummy data. We'll remove these later on
-    // during the build.
-    title := "O snail"
-    content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-    expires := 7
+	// during the build.
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
 
-    // Pass the data to the SnippetModel.Insert() method, receiving the
-    // ID of the new record back.
-    id, err := app.snippets.Insert(title, content, expires)
-    if err != nil {
-        app.serverError(w, err)
-        return
-    }
+	// Pass the data to the SnippetModel.Insert() method, receiving the
+	// ID of the new record back.
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-    // Redirect the user to the relevant page for the snippet.
-    http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	// Redirect the user to the relevant page for the snippet.
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
